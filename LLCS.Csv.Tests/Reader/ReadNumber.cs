f73a56b877ef7d050018df304cf792d8736bec04
TheAIBot <andreasuranus@hotmail.com>
2022-03-19 14:42:37 +0100

    Add more tests

--- LLCS.Csv.Tests/Reader/ReadNumber.cs
+++ LLCS.Csv.Tests/Reader/ReadNumber.cs
@@ -1,11 +1,8 @@
 using LLCS.Csv.Reader;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace LLCS.Csv.Tests.Reader
@@ -13,25 +10,6 @@ namespace LLCS.Csv.Tests.Reader
 
     public abstract class ReadNumber<T> where T : IFormattable
     {
-        private static object[] CreateRecord(string cultureName, params int[] intValues)
-        {
-            T[] values = intValues.Select(x => Clamp(x)).Select(x => CastHelper<T>.CastTo(x)).ToArray();
-            CultureInfo culture = CultureInfo.GetCultureInfo(cultureName);
-            string csv = string.Join(culture.TextInfo.ListSeparator, values.Select(x => x.ToString("N0", culture)));
-
-            return new object[] { csv, cultureName, values };
-        }
-
-        private static int Clamp(int value)
-        {
-            if (!Numeric<T>.IsSigned)
-            {
-                return 0;
-            }
-
-            return value;
-        }
-
         private static string CreateCsv(string cultureName, params int[] values)
         {
             CultureInfo culture = CultureInfo.GetCultureInfo(cultureName);
@@ -43,18 +21,40 @@ namespace LLCS.Csv.Tests.Reader
             return string.Join(Environment.NewLine, records);
         }
 
-        [Fact]
-        public void ReadWithSerializer1()
+        public static TheoryData<int, string> DifferentRecordNumbersWithDifferentCultures()
         {
-            string culture = "da-DK";
-            string csv = CombineCsvRecords(
-                CreateCsv(culture, 1),
-                CreateCsv(culture, 2),
-                CreateCsv(culture, 3),
-                CreateCsv(culture, 4),
-                CreateCsv(culture, 5),
-                CreateCsv(culture, 6));
-            OneValueStruct<T>[] expectedValues = Enumerable.Range(1, 6).Select(x => new OneValueStruct<T>(CastHelper<T>.CastTo(x))).ToArray();
+            string[] cultures = new string[]
+            {
+                "da-DK",
+                "en-US",
+            };
+
+            int[] records = new int[]
+            {
+                1,
+                10,
+                100
+            };
+
+            var data = new TheoryData<int, string>();
+            foreach (var culture in cultures)
+            {
+                foreach (var recordsCount in records)
+                {
+                    data.Add(recordsCount, culture);
+                }
+            }
+
+            return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(DifferentRecordNumbersWithDifferentCultures))]
+        public void ReadWithSerializerOneNumberPerRecord(int records, string culture)
+        {
+            string[] individualRecords = Enumerable.Range(1, records).Select(x => CreateCsv(culture, x)).ToArray();
+            string csv = CombineCsvRecords(individualRecords);
+            OneValueStruct<T>[] expectedValues = Enumerable.Range(1, records).Select(x => new OneValueStruct<T>(CastHelper<T>.CastTo(x))).ToArray();
             using CsvReader<OneValueStruct<T>> reader = CsvReader<OneValueStruct<T>>.FromString(csv, culture);
 
             OneValueStruct<T>[] actualValues = reader.ToArray();
@@ -62,18 +62,13 @@ namespace LLCS.Csv.Tests.Reader
             Assert.Equal(expectedValues, actualValues);
         }
 
-        [Fact]
-        public void ReadWithSerializer2()
+        [Theory]
+        [MemberData(nameof(DifferentRecordNumbersWithDifferentCultures))]
+        public void ReadWithSerializerTwoNumbersPerRecord(int records, string culture)
         {
-            string culture = "da-DK";
-            string csv = CombineCsvRecords(
-                CreateCsv(culture, 1, 2),
-                CreateCsv(culture, 2, 3),
-                CreateCsv(culture, 3, 4),
-                CreateCsv(culture, 4, 5),
-                CreateCsv(culture, 5, 6),
-                CreateCsv(culture, 6, 7));
-            TwoValueStruct<T, T>[] expectedValues = Enumerable.Range(1, 6).Select(x => new TwoValueStruct<T, T>(CastHelper<T>.CastTo(x), CastHelper<T>.CastTo(x + 1))).ToArray();
+            string[] individualRecords = Enumerable.Range(1, records).Select(x => CreateCsv(culture, x, x + 1)).ToArray();
+            string csv = CombineCsvRecords(individualRecords);
+            TwoValueStruct<T, T>[] expectedValues = Enumerable.Range(1, records).Select(x => new TwoValueStruct<T, T>(CastHelper<T>.CastTo(x), CastHelper<T>.CastTo(x + 1))).ToArray();
             using CsvReader<TwoValueStruct<T, T>> reader = CsvReader<TwoValueStruct<T, T>>.FromString(csv, culture);
 
             TwoValueStruct<T, T>[] actualValues = reader.ToArray();
